c476d524bb01d5fccdbe4e43654b563f427f5ab5
Jacob M. Smith <jmsmith6101@att.net>
2022-11-10 16:49:06 -0500

    finish routine function

--- src/pages/RoutineMaker.tsx
+++ src/pages/RoutineMaker.tsx
@@ -7,6 +7,8 @@ const RoutineMaker: React.FC = () => {
   //TODO
   // implement posting routine to database
   //    get data (user id, name, array of workouts)
+  //    use data to create a routien object
+  //    pass that object to createRoutine function from db.ts
   
   const finishRoutine = async () => {
     // on finish save routine and post to database
