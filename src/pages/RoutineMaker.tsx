d7cbd19da6c016d291d32e21bc4f88c2ee8fddba
Jacob M. Smith <jmsmith6101@att.net>
2022-11-11 18:38:11 -0500

    add correct imports

--- src/pages/RoutineMaker.tsx
+++ src/pages/RoutineMaker.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonButton } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './RoutineMaker.css';
 
