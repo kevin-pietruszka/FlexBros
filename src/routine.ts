0eee3e1eb3765fe9e4e4485ee0d082efee82371c
Kevin Pietruszka <kevinpiet13@gmail.com>
2022-11-30 16:20:46 -0500

    Changes database structure

--- src/routine.ts
+++ src/routine.ts
@@ -30,26 +30,17 @@ export class Exercise {
 
     exerciseName:string;
     userID:string;
-    history: HistoryItem[]
+    baseline: Set[];
+    history: any;
 
-    constructor(name:string, id:string, history:HistoryItem[]) {
+    constructor(name:string, id:string, baseline:Set[], history: any) {
         this.exerciseName = name;
         this.userID = id;
+        this.baseline = baseline;
         this.history = history;
     }
 }
 
-export class HistoryItem {
-    startDate: string;
-    endDate: string;
-    sets: Set[];
-
-    constructor(start:string, end:string, sets:Set[]) {
-        this.startDate = start;
-        this.endDate = end;
-        this.sets = sets;
-    }
-}
 
 export class Set {
     reps: number;
@@ -69,23 +60,18 @@ const sets2 = [new Set(8, 70), new Set(8, 70), new Set(8, 70)]
 const sets3 = [new Set(8, 80), new Set(8, 80), new Set(8, 80)]
 const sets4 = [new Set(8, 100), new Set(8, 100), new Set(8, 100)]
 
-const h1 = [new HistoryItem("11/1", "", sets1)]
-const h2 = [new HistoryItem("11/1", "", sets2)]
-const h3 = [new HistoryItem("11/1", "", sets3)]
-const h4 = [new HistoryItem("11/1", "", sets4)]
+const e1 = new Exercise("Bench", userID, sets1, {});
+const e2 = new Exercise("ShoulderPress", userID, sets2, {});
 
-const e1 = new Exercise("Bench", userID, h1);
-const e2 = new Exercise("ShoulderPress", userID, h2);
+const e = [e1, e2];
 
-const e = [e1, e2]
+const p1 = new Exercise("Rows", userID, sets3, {});
+const p2 = new Exercise("Pull Ups", userID, sets4, {});
 
-const p1 = new Exercise("Rows", userID, h3)
-const p2 = new Exercise("Pull Ups", userID, h4)
+const p = [p1, p2];
 
-const p = [p1, p2]
+const w1 = new Workout("Push", userID, ['MON', 'THU'], e);
+const w2 = new Workout("Pull", userID, ['TUE', 'FRI'], p);
 
-const w1 = new Workout("Push", userID, ['MON', 'THU'], e)
-const w2 = new Workout("Pull", userID, ['TUE', 'FRI'], p)
-
-const w = [w1, w2]
-export const example = new Routine("brosplit", userID, w)
+const w = [w1, w2];
+export const example = new Routine("Gerneric Routine", userID, w);
