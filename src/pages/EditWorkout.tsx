f405701ebf776d28ffc4b840c2bf213774f50f00
Jacob M. Smith <jmsmith6101@att.net>
2022-12-01 17:03:35 -0500

    routes to create workout on add click

--- src/pages/EditWorkout.tsx
+++ src/pages/EditWorkout.tsx
@@ -12,19 +12,24 @@ const EditWorkout: React.FC = () => {
     return (
         <IonPage>
             <IonHeader>
-                <IonToolbar>Edit Workout</IonToolbar>
+                <IonToolbar class="ion-text-center">Edit Workout</IonToolbar>
             </IonHeader>
             <IonContent fullscreen>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">Edit Workout</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
                 <IonCard>
                     <IonList>
                         <IonItem>
-                            <IonTitle>Workout name</IonTitle>
+                        <IonInput placeholder='Workout name'></IonInput>
                         </IonItem>
                         <IonItem>
                             begin list of exercises in workout
                         </IonItem>
                         <IonItem>
-                            <IonButton>Add</IonButton>
+                            <IonButton routerLink='/CreateWorkout'>Add</IonButton>
                             <IonButton>Finish</IonButton>
                         </IonItem>
                     </IonList>
