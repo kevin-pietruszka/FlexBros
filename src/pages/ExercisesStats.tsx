4eb8de6126cf7cb50bc03ec8a51fb9015f239c7a
Jacob M. Smith <jmsmith6101@att.net>
2022-12-01 15:45:58 -0500

    removed css import

--- src/pages/ExercisesStats.tsx
+++ src/pages/ExercisesStats.tsx
@@ -1,7 +1,6 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonInput, IonButton } from '@ionic/react';
 import { useEffect, useState } from 'react';
 import { auth } from '../firebase';
-import './ExerciseStats.css';
 
 
 const ExerciseStats: React.FC = () => {
