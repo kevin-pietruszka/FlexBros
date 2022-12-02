f239f571f310f8cc773f5b1365ab49951375265a
Kevin Pietruszka <kevinpiet13@gmail.com>
2022-12-02 15:20:46 -0500

    Creates progress graph

--- src/pages/ExercisesStats.tsx
+++ src/pages/ExercisesStats.tsx
@@ -1,5 +1,6 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonInput, IonButton, IonSelect, IonSelectOption } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonItem, IonSelect, IonSelectOption, IonCard } from '@ionic/react';
 import { useEffect, useState } from 'react';
+import ExerciseGraph from '../components/ExerciseGraph';
 import { getUserExercises, getExerciseHistory } from '../db';
 import { auth } from '../firebase';
 
@@ -10,38 +11,98 @@ const ExerciseStats: React.FC = () => {
     const [options, setOptions] = useState(['']);
     const [loading, setLoading] = useState(false);
     const [selectedExercise, setSelectedExercise] = useState("");
+    const [selectedAxis, setSelectedAxis] = useState('reps');
     const [historyData, setHistoryData] = useState({});
     const [loadedHistory, setloadedHistory] = useState(false);
 
     useEffect(() => {
 
         if (auth.currentUser != null) {
+
             setUserID(auth.currentUser.uid);
-            getUserExercises(userID).then( (results) => {
-                setOptions(arr => [...results]);
-                setLoading(true);
-            })
+            
         } else {
-            setUserID("A4A2aPnIz2VH39FsbGkPwZnzYM43");
-            getUserExercises("A4A2aPnIz2VH39FsbGkPwZnzYM43").then( (results) => {
-                setOptions(arr => [...results]);
-                setLoading(true);
-            })
+
+            setUserID('KznJACOrQoOeBo4SrZywbPc6KE72');
+            
         }
         
     }, []);
 
-    const processHistory  = (history: any, start: string, days:string[]) => {
+    useEffect(() => {
 
-    }
+        if (userID === '') return;
+
+        getUserExercises(userID).then( (results) => {
+            setOptions(arr => [...results]);
+            setLoading(true);
+        })
+    
+    }, [userID])
 
     useEffect(() => {
-      console.log(selectedExercise);
-    }, [selectedExercise])
+
+        if (userID === '' || selectedExercise === '') return;
+
+        const processHistory  = (history: any, start: string, days:string[]) => {
+    
+            let data = Object.entries(history).map(([date, setInfo]) => {
+                
+                let yaxis = 0;
+                
+                if (setInfo instanceof Array) {
+                    for (let i = 0; i < setInfo.length; i++) {
+
+                        if (selectedAxis === 'reps') {
+                            yaxis += setInfo[i].reps
+                        } else if (selectedAxis === 'weight') {
+                            yaxis += setInfo[i].weight
+                        }
+
+                    }
+
+                    if (selectedAxis === 'weight') {
+                        yaxis /= setInfo.length;
+                    }
+                }
+    
+                return ({
+                    label: date,
+                    value: yaxis
+                })
+            })
+
+            data = data.sort((a, b) => {
+
+                const date1 = new Date(a.label);
+                const date2 = new Date(b.label);
+
+                if (date1 < date2) {
+                    return -1;
+                } else if (date1 > date2) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            })
+
+            setHistoryData(data);
+            setloadedHistory(true);
+        }
+
+        getExerciseHistory(userID, selectedExercise)
+            .then((res)=>{processHistory(res?.exercise.history, res?.start, res?.days)})
+            .catch((err) => console.log(err))
+    
+    }, [userID, selectedExercise, selectedAxis])
     
 
     const exerciseSelect = (e: any) => {
         setSelectedExercise(e.detail.value)
+    }
+
+    const progressSelect = (e:any) => {
+        setSelectedAxis(e.detail.value);
     }
 
     return (
@@ -55,6 +116,10 @@ const ExerciseStats: React.FC = () => {
             </IonHeader>
             <IonContent>
 
+                {
+                    loadedHistory ? <ExerciseGraph chartData={historyData}> </ExerciseGraph> : <IonCard> </IonCard>
+                }
+
                 <IonItem>
                     {loading ? 
                         <IonSelect onIonChange={exerciseSelect} interface="popover" placeholder="Select exercise">
@@ -64,6 +129,17 @@ const ExerciseStats: React.FC = () => {
                         </IonSelect>
                         : <IonSelect></IonSelect>
                     }
+                </IonItem>
+
+                <IonItem>
+                    <IonSelect onIonChange={progressSelect} interface="popover" placeholder="Select exercise">
+                        <IonSelectOption value='reps'>
+                            Reps
+                        </IonSelectOption>
+                        <IonSelectOption value='weight'>
+                            Weight
+                        </IonSelectOption>
+                    </IonSelect>
                 </IonItem>
 
             </IonContent>
