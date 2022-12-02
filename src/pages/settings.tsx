ffa856ba4f461633f342eba56c77c2e6622f9a39
Kevin Pietruszka <kevinpiet13@gmail.com>
2022-12-02 15:52:33 -0500

    Creates Settings Componenet

---
+++ src/pages/settings.tsx
@@ -0,0 +1,19 @@
+import { IonContent, IonHeader, IonPage, IonTitle } from "@ionic/react"
+
+const Settings: React.FC = () => {
+
+
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonTitle>
+                    Settings
+                </IonTitle>
+            </IonHeader>
+            
+            <IonContent>
+    
+            </IonContent>
+        </IonPage>
+    )
+}
